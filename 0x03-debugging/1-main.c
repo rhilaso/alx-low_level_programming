1cfb068da0739ff980ef78466ddf956ba578470a
Rhilaso Dokong <rhilasosilas@yahoo.com>
2022-12-15 17:18:38 -0800

    Second debugging attempt

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,7 +3,7 @@
 /**
  * * main - causes an infinite loop
  * * Return: 0
- * */
+ */
 
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 	while (i < 10)
 	{
 		putchar(i);
-										        }
+	}
 
 
 	printf("Infinite loop avoided! \\o/\n");
