a43609c092817edf40ccb8037e97723b8508a624
Rhilaso Dokong <rhilasosilas@yahoo.com>
2022-12-15 16:32:46 -0800

    Debugging task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main- a function that print positive or negative integer
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i=0;
+	positive_or_negative(i);
+
+	return (0);
+}
