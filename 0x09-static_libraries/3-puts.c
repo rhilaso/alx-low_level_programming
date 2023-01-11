6f2c2da333a1bd9b7c455ec67f6130819f3ca3c8
Rhilaso Dokong <rhilasosilas@yahoo.com>
2023-01-11 06:26:37 -0800

    The real introduction to ALX coding

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ ** main - Entry point
+ ** puts - an executable function or statement
+ ** Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
