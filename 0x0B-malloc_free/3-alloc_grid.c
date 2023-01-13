2428133e34ef589df364795ddf512e3a2b8c9826
Rhilaso Dokong <rhilasosilas@yahoo.com>
2023-01-13 08:05:49 -0800

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ ** alloc_grid - this function create 2 dimensional array of int(s).
+ ** @width: input character for width
+ ** @height: ibput character for height
+ **
+ ** Return: returns 2 dimensional array of int(s).
+ */
+int **alloc_grid(int width, int height)
+{
+	int iterator, iterator_;
+	int **newArray;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	newArray = (int **)malloc(sizeof(int *) * height);
+
+	if (newArray == NULL)
+	{
+		free(newArray);
+		return (NULL);
+	}
+
+	for (iterator = 0; iterator < height; iterator++)
+	{
+		newArray[iterator] = (int *)malloc(sizeof(int) * width);
+
+			if (newArray[iterator] != NULL)
+			{
+				for (iterator_ = 0; iterator_ < width; iterator_++)
+					newArray[iterator][iterator_] = 0;
+			} else
+			{
+				while (iterator >= 0)
+				{
+					free(newArray[iterator]);
+					iterator--;
+				}
+
+				free(newArray);
+				return (NULL);
+
+			}
+	}
+
+	return (newArray);
+}
