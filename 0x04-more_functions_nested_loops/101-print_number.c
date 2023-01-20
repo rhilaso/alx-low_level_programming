43700da2c577b705690e5dff95056bc197217752
Rhilaso Dokong <rhilasosilas@yahoo.com>
2023-01-20 08:03:34 -0800

    extra task 2

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ ** print_number - a function that prints an integer
+ ** @n: input character
+ ** Return: Empty
+ */
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -a;
+	}
+
+	if (a > 9)
+		print_number(a / 10);
+
+	_putchar(a % 10 + '0');
+}
